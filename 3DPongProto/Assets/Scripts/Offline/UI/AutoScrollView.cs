b9781f3ee10965537fe7435b1c92279a13b7567f
Estefalis <Estefalis@web.de>
2024-08-27 20:25:59 +0200

    Security Save

---
+++ 3DPongProto/Assets/Scripts/Offline/UI/AutoScrollView.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThreeDeePongProto.Offline.UI
+{
+	public class AutoScrollView : MonoBehaviour
+	{
+		//TODO: OnActivation each ScrollRect shall register itself on the MenuManager/MenuNavigation script.
+	}
+}
