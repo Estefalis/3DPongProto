d2e1d04ab44ab913001a447ed2442899c13effeb
Estefalis <Estefalis@web.de>
2026-04-13 21:22:37 +0200

    Fallback - Pre Ball Script Rework

---
+++ 3DPongProto/Assets/Scripts/Offline/Ball/BallController.cs
@@ -0,0 +1,36 @@
+using System;
+using ThreeDeePongProto.Shared.AudioManagement;
+using ThreeDeePongProto.Shared.Managers;
+using ThreeDeePongProto.Shared.PlayerCharacter;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    #region State Management
+
+    #endregion
+
+    #region Physics & Collision Logic
+
+    #endregion
+}
